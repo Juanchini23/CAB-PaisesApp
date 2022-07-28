df655554ab37b4f9ebbdf1549171f1c3c557a1d7
Juanchini23 <jmanchini@alumno.unlam.edu.ar>
2022-07-27 21:51:05 -0300

    listort

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -4,18 +4,26 @@ import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+  templateUrl: './por-pais.component.html',
+  styles: [
+    `li{
+      cursor: pointer;
+    }`
+  ]
 })
 export class PorPaisComponent {
 
   termino :string = "";
   hayError :boolean = false;
   paises :Country[] = [];
+  paisesSugeridos :Country[] = [];
+  mostrarSugerencias :boolean = false;
 
   constructor(private servicePais :PaisService) { }
 
 
   buscar(termino :string){
+    this.mostrarSugerencias = false;
     this.hayError=false;
     this.termino=termino;
     
@@ -30,7 +38,16 @@ export class PorPaisComponent {
 
   sugerencias(termino :string){
     this.hayError=false;
-    //this.buscar(termino);
-    // crear sugerencias
+    this.termino=termino;
+    this.mostrarSugerencias=true;
+
+    this.servicePais.buscarPais(termino)
+      .subscribe(paises => this.paisesSugeridos=paises.splice(0,5),
+      (err) => this.paisesSugeridos=[]);
+    
   } 
+
+  buscarSugerido(termino :string){
+    this.buscar(termino);
+  }
 }
